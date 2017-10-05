7eaf1fd2827d0a0a2b975bf8407bdd0ffbbc6bc7
MileSacul <11881208+lashower@users.noreply.github.com>
2017-10-05 13:20:16 -0400

    Create loadNode.sh

---
+++ loadNode.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#wget https://github.com/lashower/RetroPie/master/loadNode.sh
+rpi-update
+apt-get full-upgrade
+apt-get upgrade
+apt-get update
+armv=''
+for item in $(uname -a); do if [[ $item == "arm"* ]]; then   armv=$item; fi; done
+nodefile=$(curl http://nodejs.org/dist/latest/ | sed 's/<[^>]*>//g' | grep "$armv" | grep 'tar.gz' | awk ' { print $1 } ')
+wget "http://nodejs.org/dist/latest/${nodefile}"
+mv ${nodefile} /opt
+cd /opt
+tar -xzf ${nodefile}
+rm ${nodefile}
+mv node-* nodejs
+rm -fr /usr/bin/node 2>/dev/null
+rm -fr /usr/bin/npm 2>/dev/null
+ln -s /opt/nodejs/bin/node /usr/bin/node
+ln -s /opt/nodejs/bin/npm /usr/bin/npm
+echo 'PATH="${PATH}:/opt/nodejs"' >> ~/.profile
+echo 'PATH="${PATH}:/opt/nodejs/bin"' >> ~/.profile
+echo 'export PATH' >> ~/.profile
+apt-get install mongo
+apt-get install libcurl4-openssl-dev
