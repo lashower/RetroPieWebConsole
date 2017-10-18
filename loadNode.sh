7c35c1e1f5b1bc4ffc27a4d372a4adc222be96d1
MileSacul <11881208+lashower@users.noreply.github.com>
2017-10-18 18:38:05 -0400

    Update loadNode.sh

--- loadNode.sh
+++ loadNode.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 apt-get remove node
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-apt-get install nodejs
-
-echo "#####################################"
-echo "## Adding application dependencies ##"
-echo "#####################################"
-apt-get -y install mongodb gcc make libssl-dev libcurl4-openssl-dev vim
-
-echo "########################"
-echo "## Installing pm2 ##"
-echo "########################"
+curl -sL https://deb.nodesource.com/setup_8.x | bash -
+apt-get -y install nodejs mongodb gcc make libssl-dev libcurl4-openssl-dev vim
 npm install -g pm2
