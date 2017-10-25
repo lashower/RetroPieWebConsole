05a0286c868297f706dcdcc8188e55b436e3671c
MileSacul <11881208+lashower@users.noreply.github.com>
2017-10-25 14:50:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-apt-get remove node
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
-apt-get -y install nodejs mongodb gcc make libssl-dev libcurl4-openssl-dev vim
-npm install -g pm2
+sudo apt-get remove node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get -y install nodejs mongodb gcc make libssl-dev libcurl4-openssl-dev vim
+sudo npm install -g pm2
 
-USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
-cd $USER_HOME
-sudo -u $SUDO_USER mkdir -p nodejs
+cd $HOME
+mkdir -p nodejs
 cd nodejs
-sudo -u $SUDO_USER git clone https://github.com/lashower/RetroPieWebConsole.git
+git clone https://github.com/lashower/RetroPieWebConsole.git
 cd $USER_HOME/nodejs/RetroPieWebConsole
-sudo -u $SUDO_USER npm install
+npm install
 cd $USER_HOME/nodejs/RetroPieWebConsole
-pm2 start app.js -f --name="RetroPieWebConsole"
+sudo pm2 start app.js -f --name="RetroPieWebConsole"
 pm2 startup
 pm2 save
 
