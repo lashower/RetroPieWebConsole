cd1e4d9c4078dc7ca9a79593920ac3e3c3e3f2c1
lashower <ezusolutions@gmail.com>
2017-11-27 21:59:50 -0500

    Adding games section, bugfixes, and more

--- public/script/scriptUpdate.js
+++ public/script/scriptUpdate.js
@@ -3,6 +3,7 @@ app.controller('ScriptUpdateController', function($scope, $http, $rootScope, Upl
     $scope.selfSure = false;
     $scope.disabled = false;
     $scope.results = [];
+    $scope.isUp = true;
     $scope.runUpdate = function() {
         $scope.sure = false;
         $scope.disabled = true;
@@ -17,6 +18,25 @@ app.controller('ScriptUpdateController', function($scope, $http, $rootScope, Upl
             $scope.disabled = false;
             $scope.results = response.data;
             console.log($scope.results);
+            $scope.isUp = false;
+            while(!$scope.isUp)
+            {
+                setTimeout(function() { $scope.checkStatus()}, 5000);
+            }
+        });
+    }
+
+    $scope.checkStatus = function() {
+        $http({
+            transformRequest: angular.identity,
+            method: 'GET',
+            url: '/getUserDetails'
+        }).then(function(response) {
+            console.log(response);
+            $scope.isUp = true;
+        }).catch(function(err) {
+            console.log(err);
+            $scope.isUp = false;
         });
     }
 });
