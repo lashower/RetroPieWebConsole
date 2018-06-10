9465fadd33ac08aa37fbe51e1a1bc0a85ad64b3c
MileSacul <11881208+lashower@users.noreply.github.com>
2018-06-10 00:14:17 -0400

    Update dev_setup.sh

--- dev-tools/dev_setup.sh
+++ dev-tools/dev_setup.sh
@@ -1,16 +1,24 @@
-echo 'Installing vim'
+echo -e '\e[1;32m### Installing vim ####\e[0m'
 sudo apt-get install vim
-echo 'Setting up /source/alias.props'
+
+echo -e '\e[1;32m### Installing tmux ###\e[0m'
+sudo apt-get install tmux
+
+echo -e '\e[1;32m    Getting tmux cheatsheet (tmux-cheatsheet.markdown) \e[0m'
+if [ ! -f tmux-cheatsheet.markdown ];then
+    wget 'https://gist.github.com/MohamedAlaa/2961058/raw/tmux-cheatsheet.markdown' &>/dev/null
+fi
+echo -e '\e[1;32m### Setting up /source/alias.props ###\e[0m'
 sudo mkdir -p /source
 sudo cp source/alias.props /source/alias.props
 if [ $(less ~/.bashrc | grep 'alias.props' | wc -l) -eq 0 ]; then
-    echo 'Adding /source/alias.props to .bashrc'
+    echo -e '\e[1;32m  Adding /source/alias.props to .bashrc\e[0m'
     sed -i '1 i\source /source/alias.props' ~/.bashrc
 else
-    echo '.bashrc already has alias call'
+    echo -e '\e[1;32m   .bashrc already has alias call\e[0m'
 fi
 
-echo 'Setting up ~/source/ tools'
+echo -e '\e[1;32m### Setting up ~/source/ tools ###\e[0m'
 mkdir -p ~/source
 cp -r source/common.props ~/source/common.props
 cp -r source/node.props ~/source/node.props
